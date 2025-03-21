243e0a2630fa2a773cf1f762c2814367d99ad5d0
keemcc <ktmmg4@umsystem.edu>
2025-03-21 16:22:46 -0500

    fixed load

--- Trees/src/main/java/org/example/Trees/TreeVisualizerController.java
+++ Trees/src/main/java/org/example/Trees/TreeVisualizerController.java
@@ -45,7 +45,7 @@ public class TreeVisualizerController {
         trees = new HashMap<>();
         trees.put("Binary Search Tree", new BinarySearchTree<>());
         trees.put("AVL Tree", new AVLTree<>());
-        trees.put("Red-Black Tree", new RedBlackTree<>());
+        trees.put("RBT", new RedBlackTree<>());
         trees.put("Min Heap", new MinHeap<>());
         trees.put("Max Heap", new MaxHeap<>());
         //trees.put("2-4 Tree", new Tree24<>());
