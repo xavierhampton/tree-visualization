8f974e6e17bc7c014a430fd93c853566e6e06bbd
Eli Holm <eliholm3@gmail.com>
2025-03-20 23:05:01 -0500

    Red black stuff

--- Trees/src/main/java/org/example/Trees/RedBlackTree.java
+++ Trees/src/main/java/org/example/Trees/RedBlackTree.java
@@ -6,13 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RedBlackTree<T extends Comparable<T>> implements Tree<T> , Serializable {
+
     private class Node implements TreeNode<T> , Serializable{
         T value;
         Node left, right;
-        int height;
+        Node parent;
+        String color;
 
         Node(T value) {
-            this.value = value; this.height = 1;
+            this.value = value;
+            this.parent = null;
+            this.color = "red";
         }
 
         @Override
@@ -24,12 +28,35 @@ public class RedBlackTree<T extends Comparable<T>> implements Tree<T> , Serializ
         @Override
         public TreeNode<T> getRight() { return right; }
 
-        public String getColor() { return null; }
+        public String getColor() { return this.color; }
     }
+
+    private Node root;
+    private Node temp;
+
+
 
     @Override
     public void insert(T value) {
+        if (this.root == null) {
+            this.root = new Node(value);
+            this.root.color = "black";
+        }
+        else {
+            //Binary Search
+            Node x = new Node(value);
+            temp = root;
+            while((temp.left != null) && (temp.right != null)) {
+                if (value.compareTo(temp.left.value) < 0) {
+                    temp = temp.left;
+                }
+                else {
+                    temp = temp.right;
+                }
+            }
 
+            insert(this.root, x);
+        }
     }
 
     @Override
@@ -38,13 +65,12 @@ public class RedBlackTree<T extends Comparable<T>> implements Tree<T> , Serializ
     }
 
     @Override
-    public boolean contains(T value) {
-        return false;
+    public boolean contains(T value) { return contains(this.root, value);
     }
 
     @Override
     public void clear() {
-
+        root = null;
     }
 
     @Override
@@ -69,6 +95,52 @@ public class RedBlackTree<T extends Comparable<T>> implements Tree<T> , Serializ
 
     @Override
     public TreeNode<T> getRoot() {
-        return null;
+        return this.root;
     }
+
+    private Node rotateLeft(Node n) {
+        Node x = n.right;
+        Node y = x.left;
+        x.left = n;
+        n.right = y;
+        n.parent = x;
+        if(y != null) {
+            y.parent = n;
+        }
+        return(x);
+    }
+
+    private Node rotateRight(Node n) {
+        Node x = n.left;
+        Node y = x.right;
+        x.right = n;
+        n.left = y;
+        n.parent = x;
+        if(y != null) {
+            y.parent = n;
+        }
+        return(x);
+    }
+
+    private Node insert(Node T, Node x) {
+        while (x != root && x.parent.color.equals("red")) {
+            if (x.parent == x.parent.parent.left) {
+                Node y = x.parent.parent.right;
+                if (y.color.equals("red")) {
+                    x.parent.color = "black";
+                    y.color = "red";
+                }
+            }
+        }
+
+
+    }
+
+
 }
+
+
+
+
+
+
