ffc0d254576631d6d6183af7b794127105c3adce
Xavier Hampton <xaviermichaelhampton@gmail.com>
2025-04-15 20:50:59 -0500

    reset branch, added assets folder with demo picture

--- Trees/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.Trees {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens org.example.Trees to javafx.fxml;
-    exports org.example.Trees;
-}
--- Trees/src/main/java/org/example/Trees/TreeNode.java
+++
@@ -1,8 +0,0 @@
-package org.example.Trees;
-//Auther: Abdelnasser Ouda
-public interface TreeNode<T> {
-    T getValue();
-    TreeNode<T> getLeft();
-    TreeNode<T> getRight();
-    String getColor();
-}
